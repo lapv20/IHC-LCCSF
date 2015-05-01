356a7f9a8ca5917e680fe8b3e598b133ebf7424e
Jesús Márquez <jjmarquez001@gmail.com>
2015-05-01 18:40:20 -0430

    Falta que la direccion sea obligatoria

--- admin/nuevaEmpresa.php
+++ admin/nuevaEmpresa.php
@@ -6,6 +6,14 @@
     <title>Nueva Sucursal</title>
 
 <script type="text/javascript">
+function validateForm() {
+    var tconvenio = document.forms["form"]["tipo_convenio"].value;
+    if (tconvenio== null || tconvenio == "-1") {
+        alert("No se ha elegido el tipo de convenio");
+        return false;
+    }
+}
+
 function validarLetras(e) 
 { 
     tecla = (document.all) ? e.keyCode : e.which; 
@@ -34,16 +42,44 @@ function validarNumeros(e)
     te = String.fromCharCode(tecla); 
     return patron.test(te); // prueba
 }
+function validarLyN(e) 
+{  
+    tecla = (document.all) ? e.keyCode : e.which; // 2
+    if (tecla==8) return true; // backspace
+    if (e.ctrlKey && tecla==86) { return true}; //Ctrl v
+    if (e.ctrlKey && tecla==67) { return true}; //Ctrl c
+    if (e.ctrlKey && tecla==88) { return true}; //Ctrl x
+    if (tecla>=96 && tecla<=105) { return true;} //numpad
+    patron = /[0-9-a-zA-Z]/; // patron
+    te = String.fromCharCode(tecla); 
+    return patron.test(te); // prueba
+}
+function validarLyNE(e) 
+{  
+    tecla = (document.all) ? e.keyCode : e.which; // 2
+    if (tecla==8) return true; // backspace
+    if (tecla==109) return true; // menos
+    if (tecla==110) return true; // punto
+    if (tecla==189) return false; // guion
+    if (tecla==32) return true; // espacio
+    if (e.ctrlKey && tecla==86) { return true}; //Ctrl v
+    if (e.ctrlKey && tecla==67) { return true}; //Ctrl c
+    if (e.ctrlKey && tecla==88) { return true}; //Ctrl x
+    if (tecla>=96 && tecla<=105) { return true;} //numpad
+    patron = /[0-9-a-zA-Z]/; // patron
+    te = String.fromCharCode(tecla); 
+    return patron.test(te); // prueba
+}
 </script>
 </head>
 <body>
 <div class="widget ">
 	<h4 class="widgettitle"> Nuevo Empresa</h4>
     <div class="widgetcontent nopadding">
-        <form class="stdform stdform2" method="post" action="empresa.php?accion=nuevo">
+        <form class="stdform stdform2" id="form" method="post" action="empresa.php?accion=nuevo" onsubmit="return validateForm()">
         	<p>
                 <label>Nombre</label>
-                <span class="field"><input type="text" id="nombre" name="nombre" class="input-xxlarge" placeholder="Nombre" /></span>
+                <span class="field"><input name="nombre" type="text" onKeyDown="return validarLetras(event)"  required class="input-large" placeholder="Nombre" /></span>
             </p>            
             <p>
                 <label>Tipo de Convenio</label>
@@ -58,7 +94,9 @@ function validarNumeros(e)
             
             <p>
                 <label>Telefono<small>Escriba Solo Números</small></label>
-                <span class="field"><input type="text" name="telefono" onkeydown="return validarNumeros(event)" required class="input-xlarge" placeholder="Telefono" /></span>
+                <span class="field">
+                    <input name="telefono" type="text" onkeydown="return validarNumeros(event)" required class="input-large" id="telefono" placeholder="Telefono">
+                </span>
             </p>
             <p>
             	<label>RIF<small>Registro de Identificación Fiscal</small></label>
@@ -69,13 +107,14 @@ function validarNumeros(e)
                         <option value="E">E</option>
                         <option value="G">G</option>
                 </select>
-                <input type="text" name="rif2" class="input-medium" placeholder="RIF" maxlength="8"/>
-                <input type="text" name="rif3" class="input-small" style="width:10px;"  maxlength="1"/>
+                
+                <input name="rif2" type="text" onKeyDown="return validarLyN(event)"  required class="input-medium" placeholder="RIF" maxlength="8"/>
+                <input name="rif3" type="text" onKeyDown="return validarN(event)"  required class="input-small"  style="width:10px;" maxlength="1"/>
                 </span>
             </p>
             <p>
                 <label>Direccion</label>
-                <span class="field"><textarea cols="80" rows="5" class="span10" name="direccion"></textarea></span>
+                <span class="field"><textarea cols="80" rows="5" class="span10" onKeyDown="return validarLyNE(event)" name="direccion"></textarea></span>
             </p>
                 </span>
             </p>

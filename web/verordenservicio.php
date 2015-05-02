709f937d05b78a35753cec76531b20e84db113fc
lapv20 <lapv1992@icloud.com>
2015-05-02 12:29:21 -0430

    Errores en Administrar Ordenes de Servicio de

--- web/verordenservicio.php
+++ web/verordenservicio.php
@@ -93,8 +93,8 @@ function anular(id){
 								<td><?php echo $estatus; ?></td>
 								<td>
 									<center>
-										<?php if($estatus!='Anulado'){?> <a href="#" onClick="MM_openBrWindow('modificarorden.php?sucursal=<?php echo $row['idsucursal'] ?>&perfil=<?php echo $row['idperfil'] ?>&idordenservicio=<?php echo $idorden ?>','modificarorden','width=805,height=500')"><li class="icon-edit"></li> </a><?php } ?>
-										<a href="#" <?php if($estatus=='Pendiente'){?> onClick="anular(<?php echo $row['idordenservicio']; ?>);"<?php }else{?> onClick="alert('No se puede anular una orden cuyo estatus no sea pendiente')"<?php } ?>> <li class="icon-trash"></li></a>
+										<?php if($estatus!='Anulado'){?> <a href="#" onClick="MM_openBrWindow('modificarorden.php?sucursal=<?php echo $rowos['idsucursal'] ?>&perfil=<?php echo $rowos['idperfil'] ?>&idordenservicio=<?php echo $idorden ?>','modificarorden','width=805,height=500')"><li class="icon-edit"></li> </a><?php } ?>
+										<a href="#" <?php if($estatus=='Pendiente'){?> onClick="anular(<?php echo $rowos['idordenservicio']; ?>);"<?php }else{?> onClick="alert('No se puede anular una orden cuyo estatus no sea pendiente')"<?php } ?>> <li class="icon-trash"></li></a>
 									</center>
 								</td>
 								</tr>
